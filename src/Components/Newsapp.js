c2884946acad510d8d3ab84ca526cbe0050d8e2b
JaYDeepBish <jaydeepbisht6@gmail.com>
2025-09-05 09:39:13 +0530

    commit

--- src/Components/Newsapp.js
+++ src/Components/Newsapp.js
@@ -4,10 +4,10 @@ import Card from './Card'
 const Newsapp = () => {
     const [search, setSearch] = useState("india");
     const [newsData, setNewsData] = useState(null)
-    const API_KEY = "9c3ed8ee95884dec979460a60f96675b";
+    const REACT_APP_NEWS_API_KEY = "9c3ed8ee95884dec979460a60f96675b";
 
     const getData = async() =>{
-        const response = await fetch(`https://newsapi.org/v2/everything?q=${search}&apiKey=${API_KEY}`);
+        const response = await fetch(`https://newsapi.org/v2/everything?q=${search}&apiKey=${REACT_APP_NEWS_API_KEY}`);
         const jsonData = await response.json();
         console.log(jsonData.articles);
         let dt = jsonData.articles.slice(0,10)
